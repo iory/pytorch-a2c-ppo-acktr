f750bd87de7e24a3ff14ad73958ba291604c8bb5
iory <ab.ioryz@gmail.com>
2017-09-19 03:48:40 +0900

    Add

--- model.py
+++ model.py
@@ -83,24 +83,33 @@ class GaussianActorCritic(nn.Module):
 
     def __init__(self, num_inputs, num_outputs, hidden=64):
         super(GaussianActorCritic, self).__init__()
-        self.affine1 = nn.Linear(num_inputs, hidden)
-        self.affine2 = nn.Linear(hidden, hidden)
+        self.fc1 = nn.Linear(num_inputs, hidden)
+        self.ab_fc1 = AddBias(hidden)
+        self.fc2 = nn.Linear(hidden, hidden)
+        self.ab_fc2 = AddBias(hidden)
 
         self.action_mean = nn.Linear(hidden, num_outputs)
+        self.ab_action_mean = AddBias(num_outputs)
         self.action_mean.weight.data.mul_(0.1)
         self.action_mean.bias.data.mul_(0.0)
         self.action_log_std = nn.Parameter(torch.zeros(1, num_outputs))
 
         self.value_head = nn.Linear(hidden, 1)
+        self.ab_value = AddBias(1)
 
     def forward(self, x, old=False):
-        x = F.tanh(self.affine1(x))
-        x = F.tanh(self.affine2(x))
+        x = F.tanh(self.fc1(x))
+        x = self.ab_fc1(x)
+        x = F.tanh(self.fc2(x))
+        x = self.ab_fc2(x)
 
         action_mean = self.action_mean(x)
+        action_mean = self.ab_action_mean(action_mean)
+
         action_log_std = self.action_log_std.expand_as(action_mean)
         action_std = torch.exp(action_log_std)
 
         value = self.value_head(x)
+        value = self.ab_value(value)
 
         return action_mean, action_log_std, action_std, value
